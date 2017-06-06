d3d5360a6ec90f9443b904936233a15ba77d27b9
libowen <libowen@cct.cn>
2017-06-06 19:47:21 +0800

    Initial Commit

---
+++ GoldmanFinanceDictionaryTests/GoldmanFinanceDictionaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  GoldmanFinanceDictionaryTests.swift
+//  GoldmanFinanceDictionaryTests
+//
+//  Created by libowen on 2017/6/6.
+//  Copyright © 2017年 libowen. All rights reserved.
+//
+
+import XCTest
+@testable import GoldmanFinanceDictionary
+
+class GoldmanFinanceDictionaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
