aec92d34bd44ff953239189b8a0891014308fe99
libowen <libowen@cct.cn>
2017-06-15 10:14:41 +0800

    优化正则，匹配度增加

--- GoldmanFinanceDictionary/TableViewController.swift
+++ GoldmanFinanceDictionary/TableViewController.swift
@@ -114,11 +114,12 @@ class TableViewController: UITableViewController {
                                 //tdStrings.append("1")
                             }
                             if index == 2 {
-                                let tdExplainArray = self.listMatches(pattern: ">.*?<", inString: tdTag)
+                                let tdExplainArray = self.listMatches(pattern: "(?<=>).*?(?=<)|(?<=>).*?(?=\\n)", inString: tdTag)
                                 var tdExplainResult: String = ""
                                 for string in tdExplainArray {
-                                    let result = self.listMatches(pattern: "(?<=>).*(?=<)", inString: string)
-                                    tdExplainResult.append(result[0])
+                                    //let result = self.listMatches(pattern: "(?<=>).*(?=<)", inString: string)
+                                    //tdExplainResult.append(result[0])
+                                    tdExplainResult.append(string)
                                 }
                                 tdStrings.append(tdExplainResult)
                                 //tdStrings.append("2")
