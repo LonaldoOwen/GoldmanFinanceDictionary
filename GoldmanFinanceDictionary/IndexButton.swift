8dd096b6349910d8231c687f322e8a9a63b11a63
mirageowen <ctyandmydogs@gmail.com>
2017-06-23 01:32:35 +0800

    完成switch button切换优化

--- GoldmanFinanceDictionary/IndexButton.swift
+++ GoldmanFinanceDictionary/IndexButton.swift
@@ -5,6 +5,10 @@
 //  Created by owen on 17/6/21.
 //  Copyright © 2017年 libowen. All rights reserved.
 //
+/**
+ 自定义UIButton
+ 
+ */
 
 import UIKit
 
@@ -22,7 +26,11 @@ class IndexButton: UIButton {
     var scale: Float = 0.0 {
         didSet {
             // 设置颜色
-            self.setTitleColor(UIColor.init(colorLiteralRed: scale, green: 0.0, blue: 0.0, alpha: 1.0), for: .normal)
+            if scale == 1 {
+                self.setTitleColor(UIColor.init(colorLiteralRed: scale, green: 0.0, blue: 0.0, alpha: 1.0), for: .normal) // 红色
+            } else {
+                self.setTitleColor(UIColor.init(colorLiteralRed: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)       // 白色
+            }
             // 设置变换
             let minScale: CGFloat = 0.7
             let trueScale: CGFloat = minScale + (1-minScale) * CGFloat(scale)
@@ -32,7 +40,8 @@ class IndexButton: UIButton {
     
     func setUp() {
         self.scale = 0.0
-        self.setTitleColor(.orange, for: .normal)
+        self.setTitleColor(.white, for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 24.0)
     }
     
     override init(frame: CGRect) {
