94f714321ddb89194b19857ee0330c8d40428e17
mirageowen <ctyandmydogs@gmail.com>
2017-07-22 00:19:26 +0800

    删除重复文件

--- GoldmanFinanceDictionary/DictionaryPageViewController.swift
+++ GoldmanFinanceDictionary/DictionaryPageViewController.swift
@@ -2,23 +2,102 @@
 //  DictionaryPageViewController.swift
 //  GoldmanFinanceDictionary
 //
-//  Created by libowen on 2017/6/15.
+//  Created by owen on 17/6/15.
 //  Copyright © 2017年 libowen. All rights reserved.
 //
 
 import UIKit
 
-class DictionaryPageViewController: UIPageViewController {
+class DictionaryPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    //
+    var pageCount: Int = 1
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //
+        view.backgroundColor = UIColor.orange
+        // 设置初始page
+        self.setViewControllers([loadVC(withPage: 0)], direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+        dataSource = self
+        delegate = self
+        /**
+         transitionStyle\navigationOrientation（getter）
+         注意：1、如果使用storyboard，需要在storyboard设置；2、如果使用代码，初始化UIPageViewController实例时创建
+        */
+        //self.transitionStyle = UIPageViewControllerTransitionStyle.scroll
+        //self.navigationOrientation = UIPageViewControllerNavigationOrientation.horizontal
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: data source
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        // 进入下一个pageVC
+        let currentPageVC: TableViewController = viewController as! TableViewController
+        var index = currentPageVC.pageIndex
+        if index == NSNotFound || index == pageCount-1 {
+            return nil
+        }
+        index += 1
+
+        return self.loadVC(withPage: index)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        // 返回上一个pageVC
+        let currentPageVC: TableViewController = viewController as! TableViewController
+        var index = currentPageVC.pageIndex
+        if index == 0 || index == NSNotFound {
+            return nil
+        }
+        index -= 1
+
+        return self.loadVC(withPage: index)
+    }
+    
+    
+    // MARK：实现indicator（小圆点）
+//    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+//        return 3
+//    }
+//
+//func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+//        return 0
+//    }
+    
+    
+    // MARK: delegate
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        print("willTransitionTo, \(pendingViewControllers.count)")
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        
+        print("didFinishAnimating, \(previousViewControllers.count)")
+        // 切换switch button
+        let dicVC: DictionaryViewController = pageViewController.parent as! DictionaryViewController
+        let index = (pageViewController.viewControllers?[0] as! TableViewController).pageIndex
+        dicVC.updateIndexButton(of: index)
+    }
+    
+    
+    // MARK: helper
+    func loadVC(withPage page: Int) -> UIViewController {
+        let listVC: TableViewController = storyboard?.instantiateViewController(withIdentifier: "ListVC") as! TableViewController
+        listVC.pageIndex = page
+        return listVC
     }
     
 
--- GoldmanFinanceDictionary/DictionaryViewController.swift
+++ GoldmanFinanceDictionary/DictionaryViewController.swift
@@ -2,18 +2,48 @@
 //  DictionaryViewController.swift
 //  GoldmanFinanceDictionary
 //
-//  Created by libowen on 2017/6/15.
+//  Created by owen on 17/6/15.
 //  Copyright © 2017年 libowen. All rights reserved.
 //
 
 import UIKit
 
-class DictionaryViewController: UIViewController {
+class DictionaryViewController: UIViewController, UIScrollViewDelegate {
+    
+    //
+    let nameArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "M", "N", "O", "P", "S", "T"];
+    let screanWidth = UIScreen.main.bounds.width
+    let screanHeight = UIScreen.main.bounds.height
+    let itemWidth = 40
+    var selectedIndex: Int = 0
+    var topScrollView: UIScrollView!
+    //var contentScrollView: UIScrollView!
+    var pageVC: DictionaryPageViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // 添加构造的顶部滚动view
+        view.addSubview(getTopScrollView())
+        topScrollView.showsHorizontalScrollIndicator = false
+        topScrollView.showsVerticalScrollIndicator = false
+        topScrollView.delegate = self
+    
+        // 添加page view
+        pageVC = storyboard?.instantiateViewController(withIdentifier: "DicPageVC") as! DictionaryPageViewController
+        self.addChildViewController(pageVC)
+        pageVC.view.frame = CGRect(x: 0, y: 60+46, width: screanWidth, height: screanHeight-60-46)
+        view.addSubview(pageVC.view)
+        pageVC.pageCount = nameArray.count
+        
+        // 默认第一个button选中
+        let defaulBtn: IndexButton = topScrollView.subviews[0] as! IndexButton
+        defaulBtn.scale = 1
+        
+        //temp
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +51,87 @@ class DictionaryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // temp
+        
+    
+    
+    // 构造顶部滚动view
+    func getTopScrollView() -> UIView {
+        // 顶部background
+        let topScrollBackView = UIView(frame: CGRect(x: 0, y: 60, width: screanWidth, height: 46))
+        topScrollBackView.backgroundColor = UIColor.yellow
+        
+        // 顶部滚动scrollView
+        topScrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: screanWidth, height: 46))
+        topScrollView.backgroundColor = UIColor.gray
+        topScrollView.contentSize = CGSize(width: (nameArray.count)*itemWidth, height: 46)
+        
+        // 顶部各button
+        for index in 0..<nameArray.count {
+            //let buttonItem: UIButton = UIButton(type: UIButtonType.custom)
+            let buttonItem: IndexButton = IndexButton()
+            buttonItem.frame = CGRect(x: index * itemWidth, y: 0, width: itemWidth, height: 46)
+            buttonItem.setTitle(nameArray[index], for: UIControlState.normal)
+            topScrollView.addSubview(buttonItem)
+            buttonItem.tag = 10+index
+            buttonItem.addTarget(self, action: #selector(switchClick), for: UIControlEvents.touchUpInside)
+        }
+        
+        topScrollBackView.addSubview(topScrollView)
+        return topScrollBackView
+    }
+    
+    @objc func switchClick(button: UIButton) {
+        print("button click: \(button.tag)")
+        // 切换index button
+        let currentBtn: IndexButton = button as! IndexButton
+        selectedIndex = topScrollView.subviews.index(of: currentBtn)!  // 获取当前点击button的index
+        updateIndexButton(of: selectedIndex)
+        // 显示index对应的VC
+        pageVC.setViewControllers([pageVC.loadVC(withPage: button.tag-10)], direction: .forward, animated: false, completion: nil)
+    }
+    
+    // 切换switch button
+    func updateIndexButton(of index: Int) {
+        print("index: \(index)")
+        // 调整button位置
+        changeSwitchButton(topScrollView.subviews[index] as! IndexButton)
+        // 调整变换
+        for (id, subBtn) in topScrollView.subviews.enumerated() {
+            let indexBtn: IndexButton = subBtn as! IndexButton
+            if id == index {
+                indexBtn.scale = 1.0
+            } else {
+                let indexBtn: IndexButton = subBtn as! IndexButton
+                indexBtn.scale = 0
+            }
+        }
+    }
+    
+    // 滚动switch button，调整位置
+    func changeSwitchButton(_ button: IndexButton) {
+        var offsetX = button.center.x - screanWidth/2
+        let offsetMax = topScrollView.contentSize.width - screanWidth
+        if offsetX < 0 {
+            offsetX = 0
+        } else if offsetX > offsetMax {
+            offsetX = offsetMax
+        }
+        topScrollView.setContentOffset(CGPoint(x: offsetX, y: 0), animated: true)
+    }
+    
+    
+    // MARK: UIScrollViewDelegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //
+        print(scrollView.contentOffset.x)
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        //
+    }
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        //
+    }
 
     /*
     // MARK: - Navigation
